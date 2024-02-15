b40b58aef81682bf8d0079848c51c58c337f19e8
clearly mine <clearlymine@pm.me>
2024-02-15 13:56:34 +0530

    feat: Implement boss creation by admin for user story #2

--- src/Game.sol
+++ src/Game.sol
@@ -5,7 +5,18 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 // TODO: Implement all user stories and one of the feature request
 contract Game is Ownable {
-    uint256 public x;
+    struct Boss {
+        string name;
+        uint256 power_left;
+    }
+    Boss public current_boss;
 
     constructor() Ownable(msg.sender) {}
+
+    function makeNewBoss(
+        string calldata _name,
+        uint256 _total_power
+    ) public onlyOwner {
+        current_boss = Boss({name: _name, power_left: _total_power});
+    }
 }
--- test/Game.t.sol
+++ test/Game.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import "../src/Game.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameTest is Test {
     Game public game;
@@ -13,5 +14,16 @@ contract GameTest is Test {
 
     function testOwnerIsSetCorrectly() public {
         assertEq(address(this), game.owner());
+    }
+
+    function testOnlyOwnerCanCreateNewBoss() public {
+        vm.prank(address(0));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                address(0)
+            )
+        );
+        game.makeNewBoss("gujju", 10000);
     }
 }
