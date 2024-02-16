615c7633e41e056ec4eaa3c7329a439e91c2ad80
clearly mine <clearlymine@pm.me>
2024-02-16 13:55:28 +0530

    test: add tests for user's character creation

--- test/Game.t.sol
+++ test/Game.t.sol
@@ -7,6 +7,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameTest is Test {
     Game public game;
+    string[] public characterNames = [
+        "Anya",
+        "Taylor",
+        "Joy",
+        "Joseph",
+        "Gordon",
+        "Lewitt",
+        "Batman",
+        "Superman",
+        "Spiderman",
+        "Ironman"
+    ];
 
     function setUp() public {
         game = new Game();
@@ -20,10 +32,77 @@ contract GameTest is Test {
         vm.prank(address(0));
         vm.expectRevert(
             abi.encodeWithSelector(
-                Ownable.OwnableUnauthorizedAccount.selector,
-                address(0)
+                Ownable.OwnableUnauthorizedAccount.selector, address(0)
             )
         );
-        game.makeNewBoss("gujju", 10000);
+        game.makeNewBoss("gujju", 10_000);
+    }
+
+    function testNewCharacterCreation() public {
+        vm.roll(50);
+        uint256 _power = uint256(
+            keccak256(
+                abi.encodePacked(
+                    blockhash(20),
+                    block.timestamp,
+                    address(1),
+                    block.prevrandao //this is 0 during testing
+                )
+            )
+        );
+
+        uint256 _nameIndex = _power % characterNames.length;
+        Game.Character memory _newChar = Game.Character({
+            name: characterNames[_nameIndex],
+            power_left: _power,
+            experience: 0,
+            created: true,
+            dead: false
+        });
+        vm.expectEmit();
+        vm.prank(address(1));
+        emit Game.NewCharacterCreated(address(1), _newChar);
+        game.createNewCharacter();
+
+        assertGt(game.getUsersCharacter(address(1)).power_left, 0);
+    }
+
+    function testUnableToCreateCharacterIfItsAlive() public {
+        vm.roll(50);
+        uint256 _power = uint256(
+            keccak256(
+                abi.encodePacked(
+                    blockhash(20),
+                    block.timestamp,
+                    address(1),
+                    block.prevrandao //this is 0 during testing
+                )
+            )
+        );
+
+        uint256 _nameIndex = _power % characterNames.length;
+        Game.Character memory _newChar = Game.Character({
+            name: characterNames[_nameIndex],
+            power_left: _power,
+            experience: 0,
+            created: true,
+            dead: false
+        });
+        vm.expectEmit();
+        vm.startPrank(address(1));
+        emit Game.NewCharacterCreated(address(1), _newChar);
+        game.createNewCharacter();
+
+        assertGt(game.getUsersCharacter(address(1)).power_left, 0);
+        vm.expectRevert(
+            abi.encodeWithSelector(Game.CharacterAlreadyCreated.selector)
+        );
+        game.createNewCharacter();
+        vm.stopPrank();
+    }
+
+    function testRevertOnGetUsersCharacterWhenItDoesntExist() public {
+        vm.expectRevert(Game.CharacterNotCreated.selector);
+        assertEq(game.getUsersCharacter(address(1)).power_left, 0);
     }
 }
