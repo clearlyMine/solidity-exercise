60d026c23afbcc1e1f269534c73c8382298f27bf
clearly mine <clearlymine@pm.me>
2024-02-19 14:22:38 +0530

    feat: add ability to attack boss thereby completing user story #4

--- src/Game.sol
+++ src/Game.sol
@@ -7,48 +7,93 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract Game is Ownable {
   struct Character {
     string name;
-    uint256 power_left;
-    uint256 experience;
+    uint64 powerLeft;
+    uint128 experience;
     bool created;
     bool dead;
   }
 
-  Character public current_boss;
-  mapping(address => Character) characters;
+  Character public currentBoss;
+  mapping(address userAddress => Character usersCharacter) public characters;
+  mapping(address userAddress => uint256 blockNumber) private working;
   address[] public activePlayers;
 
   string[] public characterNames =
     ["Anya", "Taylor", "Joy", "Joseph", "Gordon", "Lewitt", "Batman", "Superman", "Spiderman", "Ironman"];
 
   error CharacterAlreadyCreated();
-  error CharacterNotCreated();
+  error CharacterNotCreated(address);
+  error CharacterAlreadyWorking();
+  // "Boss is still alive"
+  error BossNotDead();
+  error BossNotCreated();
+  error BossIsDead();
+  //  Name cannot be empty
+  error EmptyNameSupplied();
 
   event NewCharacterCreated(address indexed creator, Character character);
+  event NewBossCreated(Character boss);
+  event BossAttacked(Character attacker);
 
   constructor() Ownable(msg.sender) {}
 
-  function makeNewBoss(string calldata _name, uint256 _total_power) external onlyOwner {
-    current_boss = Character({name: _name, power_left: _total_power, experience: 0, created: true, dead: false});
+  function makeNewBoss(string memory _name, uint64 _totalPower) public onlyOwner {
+    _revertOnAliveBoss();
+
+    currentBoss = Character({name: _name, powerLeft: _totalPower, experience: 0, created: true, dead: false});
+    emit NewBossCreated(currentBoss);
   }
 
-  function makeNewBoss(string calldata _name) external onlyOwner {
-    current_boss = Character({name: _name, power_left: _getRandomPower(), experience: 0, created: true, dead: false});
+  function makeNewBossWithRandomPowers(string calldata _name) external onlyOwner {
+    _revertOnAliveBoss();
+    _makeNewBossWithRandomPowers(_name);
+  }
+
+  function _makeNewBossWithRandomPowers(string calldata _name) internal {
+    _revertOnAliveBoss();
+    makeNewBoss(_name, _getRandomPower() * 10);
+  }
+
+  function _makeNewRandomBoss() internal {
+    _revertOnAliveBoss();
+    uint64 _pow = _getRandomPower() * 10;
+    string memory _name = characterNames[_pow % characterNames.length];
+    makeNewBoss(_name, _pow);
+  }
+
+  function _revertOnUninitializedBoss() internal view {
+    if (!currentBoss.created) {
+      revert BossNotCreated();
+    }
+  }
+
+  function _revertOnAliveBoss() internal view {
+    if (currentBoss.created && !currentBoss.dead && currentBoss.powerLeft != uint256(0)) {
+      revert BossNotDead();
+    }
+  }
+
+  function _revertOnDeadBoss() internal view {
+    if (currentBoss.dead || currentBoss.powerLeft == uint256(0)) {
+      revert BossIsDead();
+    }
   }
 
   function addToCharacterNamesList(string calldata _newName) external onlyOwner {
-    require(bytes(_newName).length != 0);
+    if (bytes(_newName).length != 0) {
+      revert EmptyNameSupplied();
+    }
     characterNames.push(_newName);
   }
 
   function createNewCharacter() external {
-    Character memory _uChar = characters[msg.sender];
-    if (_uChar.created) {
+    if (characters[msg.sender].created) {
       revert CharacterAlreadyCreated();
     }
-    uint256 _power = _getRandomPower();
+    uint64 _power = _getRandomPower();
     uint256 _nameIndex = _power % characterNames.length;
     Character memory _newChar =
-      Character({name: characterNames[_nameIndex], power_left: _power, experience: 0, created: true, dead: false});
+      Character({name: characterNames[_nameIndex], powerLeft: _power, experience: 0, created: true, dead: false});
     characters[msg.sender] = _newChar;
     activePlayers.push(msg.sender);
     emit NewCharacterCreated(msg.sender, _newChar);
@@ -57,13 +102,49 @@ contract Game is Ownable {
   function getUsersCharacter(address adr) external view returns (Character memory) {
     Character memory ch = characters[adr];
     if (!ch.created) {
-      revert CharacterNotCreated();
+      revert CharacterNotCreated(adr);
     }
     return ch;
   }
 
-  function _getRandomPower() internal view returns (uint256) {
-    return
-      uint256(keccak256(abi.encodePacked(blockhash(block.number - 30), block.timestamp, msg.sender, block.prevrandao)));
+  function attackBoss() external {
+    _revertOnUninitializedBoss();
+    _revertOnDeadBoss();
+    Character storage _uChar = characters[msg.sender];
+    if (!_uChar.created) {
+      revert CharacterNotCreated(msg.sender);
+    }
+    if (working[msg.sender] >= block.number) {
+      revert CharacterAlreadyWorking();
+    }
+
+    emit BossAttacked(_uChar);
+    working[msg.sender] = block.number;
+    uint64 charP = _uChar.powerLeft;
+    uint64 bP = currentBoss.powerLeft;
+
+    if (bP <= charP) {
+      currentBoss.powerLeft = 0;
+      currentBoss.dead = true;
+    } else {
+      currentBoss.powerLeft -= charP;
+    }
+    bP /= 100;
+    if (charP <= bP) {
+      _uChar.powerLeft = 0;
+      _uChar.dead = true;
+    } else {
+      _uChar.powerLeft -= bP;
+    }
+
+    if (currentBoss.dead) {
+      _makeNewRandomBoss();
+    }
+  }
+
+  function _getRandomPower() internal view returns (uint64) {
+    return uint64(
+      bytes8(keccak256(abi.encodePacked(blockhash(block.number - 30), block.timestamp, msg.sender, block.prevrandao)))
+    ) / 10;
   }
 }
--- test/Game.t.sol
+++ test/Game.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
-import "../src/Game.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Test} from "forge-std/Test.sol";
+import {Game} from "../src/Game.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameTest is Test {
   Game public game;
@@ -12,6 +12,7 @@ contract GameTest is Test {
 
   function setUp() public {
     game = new Game();
+    vm.roll(50);
   }
 
   function testOwnerIsSetCorrectly() public {
@@ -21,53 +22,166 @@ contract GameTest is Test {
   function testOnlyOwnerCanCreateNewBoss() public {
     vm.prank(address(0));
     vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(0)));
-    game.makeNewBoss("gujju", 10_000);
+    game.makeNewBossWithRandomPowers("gujju");
   }
 
   function testNewCharacterCreation() public {
-    vm.roll(50);
-    uint256 _power = uint256(
-      keccak256(
-        abi.encodePacked(
-          blockhash(20),
-          block.timestamp,
-          address(1),
-          block.prevrandao //this is 0 during testing
-        )
-      )
-    );
+    Game.Character memory _newChar = Game.Character({
+      name: "Superman",
+      powerLeft: 1_730_191_093_711_958_967,
+      experience: 0,
+      created: true,
+      dead: false
+    });
 
-    uint256 _nameIndex = _power % characterNames.length;
-    Game.Character memory _newChar =
-      Game.Character({name: characterNames[_nameIndex], power_left: _power, experience: 0, created: true, dead: false});
-    vm.expectEmit();
     vm.prank(address(1));
+    vm.expectEmit();
     emit Game.NewCharacterCreated(address(1), _newChar);
     game.createNewCharacter();
-
-    assertGt(game.getUsersCharacter(address(1)).power_left, 0);
   }
 
-  function testUnableToCreateCharacterIfItsAlive() public {
-    vm.roll(50);
-    uint256 _power = uint256(keccak256(abi.encodePacked(blockhash(20), block.timestamp, address(1), block.prevrandao)));
-
-    uint256 _nameIndex = _power % characterNames.length;
-    Game.Character memory _newChar =
-      Game.Character({name: characterNames[_nameIndex], power_left: _power, experience: 0, created: true, dead: false});
-    vm.expectEmit();
+  function testDontReCreateCharacter() public {
     vm.startPrank(address(1));
-    emit Game.NewCharacterCreated(address(1), _newChar);
     game.createNewCharacter();
 
-    assertGt(game.getUsersCharacter(address(1)).power_left, 0);
     vm.expectRevert(abi.encodeWithSelector(Game.CharacterAlreadyCreated.selector));
     game.createNewCharacter();
     vm.stopPrank();
   }
 
   function testRevertOnGetUsersCharacterWhenItDoesntExist() public {
-    vm.expectRevert(Game.CharacterNotCreated.selector);
-    assertEq(game.getUsersCharacter(address(1)).power_left, 0);
+    vm.expectRevert(abi.encodeWithSelector(Game.CharacterNotCreated.selector, address(1)));
+    game.getUsersCharacter(address(1));
+  }
+
+  function testCanAttackBoss() public {
+    //create boss
+    game.makeNewBossWithRandomPowers("gujju");
+    Game.Character memory _boss =
+      Game.Character({name: "gujju", powerLeft: 18_417_920_234_273_413_000, experience: 0, created: true, dead: false});
+    vm.roll(52);
+
+    //create character
+    vm.startPrank(address(1));
+    game.createNewCharacter();
+    Game.Character memory _char =
+      Game.Character({name: "Joy", powerLeft: 341_403_779_608_662_062, experience: 0, created: true, dead: false});
+
+    //attack boss
+    vm.expectEmit();
+    emit Game.BossAttacked(_char);
+    game.attackBoss();
+
+    Game.Character memory _newChar = game.getUsersCharacter(address(1));
+    (, uint256 bossPowerLeft,,, bool bossDead) = game.currentBoss();
+    assertEq(bossPowerLeft, _boss.powerLeft - _char.powerLeft);
+    assert(!bossDead);
+    assertEq(_newChar.powerLeft, _char.powerLeft - _boss.powerLeft / 100);
+    assert(!_newChar.dead);
+  }
+
+  function testCanAttackBossInSubsequentBlocks() public {
+    //create boss
+    game.makeNewBossWithRandomPowers("gujju");
+    vm.roll(52);
+
+    //create character
+    vm.startPrank(address(1));
+    game.createNewCharacter();
+    Game.Character memory _char =
+      Game.Character({name: "Joy", powerLeft: 341_403_779_608_662_062, experience: 0, created: true, dead: false});
+
+    //attack boss
+    vm.expectEmit();
+    emit Game.BossAttacked(_char);
+    game.attackBoss();
+
+    //re-attack boss
+    vm.roll(53);
+    _char = Game.Character({name: "Joy", powerLeft: 157_224_577_265_927_932, experience: 0, created: true, dead: false});
+    vm.expectEmit();
+    emit Game.BossAttacked(_char);
+    game.attackBoss();
+
+    Game.Character memory _newChar = game.getUsersCharacter(address(1));
+    (, uint256 bossPowerLeft,,, bool bossDead) = game.currentBoss();
+    assertEq(bossPowerLeft, 17_919_291_877_398_823_006);
+    assert(!bossDead);
+    assertEq(_newChar.powerLeft, 0);
+    assert(_newChar.dead);
+  }
+
+  function testCannotAttackBossWithoutCharacter() public {
+    //create boss
+    game.makeNewBossWithRandomPowers("gujju");
+
+    //create character
+    vm.roll(52);
+    vm.startPrank(address(1));
+
+    //attack boss
+    vm.expectRevert(abi.encodeWithSelector(Game.CharacterNotCreated.selector, address(1)));
+    game.attackBoss();
+  }
+
+  function testCannotAttackBossWhenBossIsNotCreated() public {
+    //create character
+    vm.roll(52);
+    vm.startPrank(address(1));
+    game.createNewCharacter();
+
+    //attack boss
+    vm.expectRevert(Game.BossNotCreated.selector);
+    game.attackBoss();
+    vm.stopPrank();
+  }
+
+  // function testCannotAttackBossWhenBossIsDead() public {
+  //   //create boss
+  //   game.makeNewBossWithRandomPowers("gujju");
+  //   Game.Character memory _boss = Game.Character({
+  //     name: "gujju",
+  //     powerLeft: 18_417_920_234_273_413_000,
+  //     experience: 0,
+  //     created: true,
+  //     dead: false
+  //   });
+  //   vm.roll(52);
+  //
+  //   //create character
+  //   vm.startPrank(address(1));
+  //   game.createNewCharacter();
+  //   Game.Character memory _char = Game.Character({
+  //     name: "Joy",
+  //     powerLeft: 341_403_779_608_662_062,
+  //     experience: 0,
+  //     created: true,
+  //     dead: false
+  //   });
+  //
+  //   //attack boss
+  //   vm.expectRevert(Game.BossIsDead.selector);
+  //   game.attackBoss();
+  //   vm.stopPrank();
+  // }
+
+  function testCannotAttackBossTwiceInTheSameBlock() public {
+    //create boss
+    game.makeNewBossWithRandomPowers("gujju");
+    vm.roll(52);
+
+    //create character
+    vm.startPrank(address(1));
+    game.createNewCharacter();
+    Game.Character memory _char =
+      Game.Character({name: "Joy", powerLeft: 341_403_779_608_662_062, experience: 0, created: true, dead: false});
+
+    //attack boss
+    vm.expectEmit();
+    emit Game.BossAttacked(_char);
+    game.attackBoss();
+
+    vm.expectRevert(Game.CharacterAlreadyWorking.selector);
+    game.attackBoss();
   }
 }
